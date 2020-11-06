bdebb6c680588a40623debfab326612a9b760afb
liudw <9999@163.com>
2020-11-06 15:48:51 +0800

    there are some thing todo

---
+++ bigdataAPI/src/main/java/com/kafka/SpecifiedOffset.java
@@ -0,0 +1,13 @@
+package com.kafka;
+
+/**
+ *  @author: liudw
+ *  @date: 2020-11-6 15:46
+ */
+
+/**
+ *  consumed from specified offset/timestamp
+ */
+public class SpecifiedOffset {
+	// TODO: 2020-11-6 从指定的offset/timestamp 开始消费数据
+}
