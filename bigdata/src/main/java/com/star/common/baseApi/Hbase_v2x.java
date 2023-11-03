2dd84ed83edab0257921ebc1a8c7092a6dadba42
liudw <dw199501@163.com>
2023-11-03 20:48:20 +0800

    2023-11-03 hbase

--- bigdata/src/main/java/com/star/common/baseApi/Hbase_v2x.java
+++ bigdata/src/main/java/com/star/common/baseApi/Hbase_v2x.java
@@ -8,6 +8,7 @@ package com.star.common.baseApi;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.Cell;
 import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.NamespaceDescriptor;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Admin;
 import org.apache.hadoop.hbase.client.ColumnFamilyDescriptor;
@@ -38,22 +39,22 @@ public class Hbase_v2x {
 
     private static Configuration conf = null;
     private static Connection conn = null;
-    private static Admin admin = null;
 
 
     // 返回 连接对象
     static {
         try {
-            // 通过 kerberos 认证连接hbase
+
             conf = HBaseConfiguration.create();
-            conf.addResource("hbase-site.xml");
-            conf.addResource("hdfs-site.xml");
-            conf.addResource("core-site.xml");
-            System.setProperty("java.security.krb5.conf", "D:\\krb5.conf");
-            UserGroupInformation.setConfiguration(conf);
-            UserGroupInformation.loginUserFromKeytab("asset_app", "D:\\asset_app.keytab");
+            //conf.addResource("hbase-site.xml");
+
+            // 通过 kerberos 认证连接hbase
+            //System.setProperty("java.security.krb5.conf", "D:\\krb5.conf");
+            //UserGroupInformation.setConfiguration(conf);
+            //UserGroupInformation.loginUserFromKeytab("asset_app", "D:\\asset_app.keytab");
+
+            // 创建Hbase 连接
             conn = ConnectionFactory.createConnection(conf);
-            admin = conn.getAdmin();
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -61,49 +62,106 @@ public class Hbase_v2x {
     }
 
 
-    public void createTable(String tableName, String columnFamily) {
+    /**
+     * 创建命名空间
+     *
+     * @param nameSpace 命名空间
+     */
+    public void createNameSpace(String nameSpace) throws IOException {
+        Admin admin = conn.getAdmin();
+        NamespaceDescriptor.Builder builder = NamespaceDescriptor.create(nameSpace);
 
         try {
-            if (admin.tableExists(TableName.valueOf(tableName))) {
+            admin.createNamespace(builder.build());
+        } catch (IOException e) {
+            System.out.println(nameSpace + " 命名空间已存在或不符合要求.");
+            e.printStackTrace();
+        }
+
+        admin.close();
+    }
+
+    /**
+     * 判断Hbase表是否存在
+     *
+     * @param nameSpace 命名空间
+     * @param tableName 表名
+     * @return true:存在 false:不存在
+     */
+    public boolean isTableExists(String nameSpace, String tableName) throws IOException {
+        Admin admin = conn.getAdmin();
+
+        boolean b = false;
+        try {
+            b = admin.tableExists(TableName.valueOf(nameSpace, tableName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return b;
+    }
+
+    /**
+     * 创建Hbase 表
+     *
+     * @param nameSpace    命名空间
+     * @param tableName    表名
+     * @param columnFamily 列族
+     * @throws IOException
+     */
+    public void createTable(String nameSpace, String tableName, String... columnFamily) throws IOException {
+
+        // 判断是否有列族(Hbase 建表至少需要一个列族)
+        if (columnFamily.length < 1) {
+            System.out.println("创建Hbase表至少需要一个列族");
+            return;
+        }
+
+        Admin admin = conn.getAdmin();
+        try {
+            if (admin.tableExists(TableName.valueOf(nameSpace, tableName))) {
+                System.out.println(nameSpace + ":" + tableName + " 表已存在");
                 return;
             }
-            // 如果表不存在则创建
-            TableName name = TableName.valueOf(tableName);
-            // 表描述器
-            TableDescriptorBuilder tdb = TableDescriptorBuilder.newBuilder(name);
-            TableDescriptor td = tdb.build();
-            // 列族描述器
-            ColumnFamilyDescriptorBuilder cfdb = ColumnFamilyDescriptorBuilder.newBuilder(Bytes.toBytes(columnFamily));
-            ColumnFamilyDescriptor cfd = cfdb.build();
-            // 添加列族
-            tdb.setColumnFamily(cfd);
-            // admin.addColumnFamily(name,cfd);
-            // 创建表
-            admin.createTable(td);
-            System.out.println("create table success");
 
+            // 表描述器
+            TableDescriptorBuilder tableDescriptorBuilder = TableDescriptorBuilder.newBuilder(TableName.valueOf(nameSpace, tableName));
+
+            // 添加列族
+            for (String f : columnFamily) {
+                ColumnFamilyDescriptorBuilder columnFamilyDescriptorBuilder = ColumnFamilyDescriptorBuilder.newBuilder(Bytes.toBytes(f));
+                // 设置一些参数
+                columnFamilyDescriptorBuilder.setMaxVersions(3);
+                // 设置列族
+                tableDescriptorBuilder.setColumnFamily(columnFamilyDescriptorBuilder.build());
+            }
+            // 创建表
+            admin.createTable(tableDescriptorBuilder.build());
+            System.out.println("create table " + nameSpace + ":" + tableName + " success.");
+            admin.close();
         } catch (IOException e) {
             System.out.println("Oops...! create table failed!");
             e.printStackTrace();
-        } finally {
-            close(admin);
         }
     }
 
     /**
      * 删除表
      *
+     * @param nameSpace 命名空间
      * @param tableName 表名
      * @return 表删除成功返回true
      */
-    public void deleteTable(String tableName) {
+    public void deleteTable(String nameSpace, String tableName) throws IOException {
+        Admin admin = conn.getAdmin();
         try {
-            if (!admin.tableExists(TableName.valueOf(tableName))) {
+            if (!admin.tableExists(TableName.valueOf(nameSpace, tableName))) {
+                System.out.println(nameSpace + ":" + tableName + " 表不存在,无法删除.");
                 return;
             }
-            admin.disableTable(TableName.valueOf(tableName));
-            admin.deleteTable(TableName.valueOf(tableName));
-            System.out.println("delete table success");
+            admin.disableTable(TableName.valueOf(nameSpace, tableName));
+            admin.deleteTable(TableName.valueOf(nameSpace, tableName));
+            System.out.println("delete " + nameSpace + ":" + tableName + " table success");
+            admin.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -113,18 +171,20 @@ public class Hbase_v2x {
      * put 写入一条数据
      *
      * @param tableName    表名
-     * @param rowkey       rowkey 名称
+     * @param nameSpace    命名空间
+     * @param rowkey       rowkey
      * @param columnFamily 列族
      * @param qualifier    列名
      * @param value        值
      * @return 写入数据成功返回true
      */
-    public boolean putRow(String tableName, String rowkey, String columnFamily, String qualifier, String value) {
+    public boolean putRow(String nameSpace, String tableName, String rowkey, String columnFamily, String qualifier, String value) {
         try {
-            Table table = conn.getTable(TableName.valueOf(tableName));
+            Table table = conn.getTable(TableName.valueOf(nameSpace, tableName));
             Put put = new Put(rowkey.getBytes());
             put.addColumn(columnFamily.getBytes(), qualifier.getBytes(), Bytes.toBytes(value));
             table.put(put);
+            System.out.println("写入数据成功");
             table.close();
         } catch (IOException e) {
             e.printStackTrace();
@@ -169,16 +229,17 @@ public class Hbase_v2x {
     /**
      * 根据rowkey获取一行数据
      *
+     * @param nameSpace 命名空间
      * @param tableName 表名
      * @param rowkey    rowkey名称
      * @return 返回Result对象
      */
-    public Result getValueByRowkey(String tableName, String rowkey) {
+    public Result getValueByRowkey(String nameSpace, String tableName, String rowkey) {
         try {
-            Table table = conn.getTable(TableName.valueOf(tableName));
+            Table table = conn.getTable(TableName.valueOf(nameSpace, tableName));
             Get get = new Get(Bytes.toBytes(rowkey));
             Result result = table.get(get);
-            if (result.size() > 1) {
+            if (result.size() > 0) {
                 return result;
             }
         } catch (IOException e) {
@@ -190,13 +251,14 @@ public class Hbase_v2x {
     /**
      * Get获取指定行指定列 的最新数据
      *
-     * @param tableName
-     * @param rowkey
+     * @param nameSpace 命名空间
+     * @param tableName 表名
+     * @param rowkey    rowkey
      * @return
      */
-    public String getCell(String tableName, String rowkey, String columnFamily, String qualifier) {
+    public String getCell(String nameSpace, String tableName, String rowkey, String columnFamily, String qualifier) {
         try {
-            Table table = conn.getTable(TableName.valueOf(tableName));
+            Table table = conn.getTable(TableName.valueOf(nameSpace, tableName));
             Get get = new Get(rowkey.getBytes());
             if (!get.isCheckExistenceOnly()) {
                 get.addColumn(columnFamily.getBytes(), qualifier.getBytes());
@@ -211,42 +273,18 @@ public class Hbase_v2x {
         return null;
     }
 
-    /**
-     * get获取指定rowkey的所有列的数据
-     *
-     * @param tableName
-     * @param rowkey
-     */
-    public void getAllCellByRowkey(String tableName, String rowkey) {
-        try {
-            Table table = conn.getTable(TableName.valueOf(tableName));
-            Get get = new Get(rowkey.getBytes());
-
-            Result result = table.get(get);
-            for (Cell cell : result.rawCells()) {
-                // String rowkey = Bytes.toString(cell.getRowArray(), cell.getRowOffset(), cell.getRowLength());
-                // 获取列族
-                String family = Bytes.toString(cell.getFamilyArray(), cell.getFamilyOffset(), cell.getFamilyLength());
-                // 获取列
-                String qualifier = Bytes.toString(cell.getQualifierArray(), cell.getQualifierOffset(), cell.getQualifierLength());
-                // 获取值(cell)
-                String value = Bytes.toString(cell.getValueArray(), cell.getValueOffset(), cell.getValueLength());
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
     /**
-     * 检索全表
+     * Scan 表
      * ResultScanner 对象里封装的就是Result 对象
      *
-     * @param tableName
+     * @param nameSpace 命名空间
+     * @param tableName 表名
      * @return
      */
-    public void scanTable(String tableName) {
+    public void scanTable(String nameSpace, String tableName) {
         try {
-            Table table = conn.getTable(TableName.valueOf(tableName));
+            Table table = conn.getTable(TableName.valueOf(nameSpace, tableName));
             Scan scan = new Scan();
             // 设置LIMIT=> 3
             scan.setLimit(3);
@@ -274,14 +312,15 @@ public class Hbase_v2x {
     /**
      * 限定 startrow 和 stoprow
      *
-     * @param tableName
+     * @param nameSpace   命名空间
+     * @param tableName   表名
      * @param startRowkey
      * @param stopRowkey
      * @return
      */
-    public ResultScanner scanByStartrowAndStoprow(String tableName, String startRowkey, String stopRowkey) {
+    public ResultScanner scanByStartrowAndStoprow(String nameSpace, String tableName, String startRowkey, String stopRowkey) {
         try {
-            Table table = conn.getTable(TableName.valueOf(tableName));
+            Table table = conn.getTable(TableName.valueOf(nameSpace, tableName));
             Scan scan = new Scan();
             scan.withStartRow(startRowkey.getBytes());
             scan.withStopRow(stopRowkey.getBytes());
@@ -297,16 +336,19 @@ public class Hbase_v2x {
      * 使用Filter 进行过滤
      * 过滤的使用 {@link com.star.hbase.ScanFilter#filterList}
      *
-     * @param tableName
+     * @param nameSpace   命名空间
+     * @param tableName   表名
      * @param startRowkey
      * @param stopRowkey
      * @return
      */
-    public ResultScanner scanByFilterList(String tableName, String startRowkey, String stopRowkey, FilterList filterList) {
+    public ResultScanner scanByFilterList(String nameSpace, String tableName, String startRowkey, String stopRowkey, FilterList filterList) {
 
         try {
-            Table table = conn.getTable(TableName.valueOf(tableName));
+            Table table = conn.getTable(TableName.valueOf(nameSpace, tableName));
             Scan scan = new Scan();
+            scan.withStartRow(startRowkey.getBytes());
+            scan.withStopRow(stopRowkey.getBytes());
             // 添加过滤器
             scan.setFilter(filterList);
             ResultScanner scanner = table.getScanner(scan);
@@ -320,13 +362,14 @@ public class Hbase_v2x {
     /**
      * 删除指定行数据
      *
-     * @param tableName
+     * @param nameSpace 命名空间
+     * @param tableName 表名
      * @param rowkey
      * @return
      */
-    public boolean deleteRow(String tableName, String rowkey) {
+    public boolean deleteRow(String nameSpace, String tableName, String rowkey) {
         try {
-            Table table = conn.getTable(TableName.valueOf(tableName));
+            Table table = conn.getTable(TableName.valueOf(nameSpace, tableName));
             Delete delete = new Delete(rowkey.getBytes());
             table.delete(delete);
         } catch (IOException e) {
@@ -338,15 +381,16 @@ public class Hbase_v2x {
     /**
      * 删除指定列数据
      *
+     * @param nameSpace
      * @param tableName
      * @param rowKey
      * @param familyName
      * @param qualifier
      * @return
      */
-    public boolean deleteColumn(String tableName, String rowKey, String familyName, String qualifier) {
+    public boolean deleteColumn(String nameSpace, String tableName, String rowKey, String familyName, String qualifier) {
         try {
-            Table table = conn.getTable(TableName.valueOf(tableName));
+            Table table = conn.getTable(TableName.valueOf(nameSpace, tableName));
             Delete delete = new Delete(Bytes.toBytes(rowKey));
             // 删除某列的内容
             delete.addColumn(Bytes.toBytes(familyName), Bytes.toBytes(qualifier));
@@ -361,19 +405,6 @@ public class Hbase_v2x {
             e.printStackTrace();
         }
         return true;
-    }
-
-    /**
-     * 关闭资源
-     *
-     * @param admin
-     */
-    private void close(Admin admin) {
-        try {
-            if (admin != null) admin.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
 
--- bigdata/src/test/java/HbaseTest.java
+++ bigdata/src/test/java/HbaseTest.java
@@ -1,6 +1,12 @@
 import com.star.common.baseApi.Hbase_v2x;
 
+import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.CellScanner;
+import org.apache.hadoop.hbase.CellUtil;
+import org.apache.hadoop.hbase.client.Result;
 import org.junit.Test;
+
+import java.io.IOException;
 
 /**
  * @author Spring
@@ -9,14 +15,43 @@ public class HbaseTest {
 
     Hbase_v2x hbase = new Hbase_v2x();
 
+    // 创建命名空间
     @Test
-    public void getValueByRowkey() {
+    public void createNameSpace() throws IOException {
+        hbase.createNameSpace("test");
+    }
 
-        hbase.getAllCellByRowkey("stockdealrecords", "03 1418903 2021");
+
+    // hbase 表是否存在
+    @Test
+    public void isTableExists() throws IOException {
+        boolean tableExists = hbase.isTableExists("bigdata", "test1019");
+        System.out.println(tableExists);
     }
 
     @Test
-    public void scanTable() {
+    public void createTable() throws IOException {
+        hbase.createTable("bigdata","test1020", "f1","f2");
+    }
+
+
+    @Test
+    public void putRow() {
+        hbase.putRow("bigdata", "test1021","00001","f1","info","hahah");
+    }
+
+    @Test
+    public void getValueByRowkey() {
+        Result result = hbase.getValueByRowkey("bigdata", "test1021", "00001");
+        Cell[] cells = result.rawCells();
+        for (Cell cell : cells) {
+            String value = new String(CellUtil.cloneValue(cell));
+            System.out.println(value);
+        }
+    }
+
+    @Test
+    public void scanTable() throws IOException {
         hbase.createTable("t2", "f");
     }
 }
